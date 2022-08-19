e5b1a674c25535a661caa0fc59d01a1ff4c51218
Luca Martinelli <martinelliluca98@gmail.com>
2022-08-19 10:52:43 +0200

    Added public organization

--- ontoim_py/__init__.py
+++ ontoim_py/__init__.py
@@ -1,7 +1,7 @@
 from .ns import *
 from ontopia_py import createGraph as cg, saveGraph
 
-VERSION = (0, 0, 6)
+VERSION = (0, 0, 7)
 
 __author__ = 'Luca Martinelli'
 __email__ = 'martinelliluca98@gmail.com'
