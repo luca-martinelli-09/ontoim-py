b65a59f2f3fc1f1aed29175eb9ac536251caa73e
Luca Martinelli <martinelliluca98@gmail.com>
2022-09-29 16:32:13 +0200

    Fixed bug

--- ontoim_py/ontoim/InvolvedPerson.py
+++ ontoim_py/ontoim/InvolvedPerson.py
@@ -4,26 +4,23 @@ from typing import TYPE_CHECKING
 
 from ..ns import *
 from .InvolvedEntity import InvolvedEntity
+from ontopia_py.cpv.Person import Person
 
 if TYPE_CHECKING:
-    from ontopia_py.cpv.Person import Person
     from rdflib import Graph
 
     from .InvolvedPersonStatus import InvolvedPersonStatus
 
 
-class InvolvedPerson(InvolvedEntity):
+class InvolvedPerson(InvolvedEntity, Person):
     __type__ = ONTOIM["InvolvedPerson"]
 
-    hasPerson: InvolvedPersonStatus = None
-    hasPerson: Person = None
+    hasInvolvedPersonStatus: InvolvedPersonStatus = None
 
     def _addProperties(self, g: Graph):
-        super()._addProperties(g)
+        super(InvolvedEntity)._addProperties(g)
+        super(Person)._addProperties(g)
 
         if self.hasInvolvedPersonStatus:
             g.add((self.uriRef, ONTOIM["hasInvolvedPersonStatus"],
                   self.hasInvolvedPersonStatus.uriRef))
-
-        if self.hasPerson:
-            g.add((self.uriRef, ONTOIM["hasPerson"], self.hasPerson.uriRef))
